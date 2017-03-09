d821c58e767c7bd7429f85fdb7bc3853b1464c97
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-09 14:14:29 +0100

    fin exercice 1

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 var a = 10;
 var b = 40;
+
+// addition
+
 function addition(a, b) {
 	return a + b;
 
 }
+
 console.log(a + b)
+
+// soustraction
+
+function soustraction(a, b) {
+	return a - b;
+}
+
+console.log(a - b )
+
+// miltiplication
+
+function multiplication(a, b) {
+	return a * b;
+}
+
+console.log(a * b)
+
+// division
+
+function multiplication(a, b) {
+ 	return a / b
+ 
+ if (b> 0) 
+ 	console.log(a / b);
+ else 
+ 	console.log("Can't divide by 0")}
+
+console.log	(a / b);
