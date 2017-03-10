fbd48c01e9a87350a718f293f42fb529810c6c76
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-10 17:07:11 +0100

    caculatrice jquery

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-var a = 10;
-var b = 40;
 
 // addition
 
 function addition(a, b) {
+ 
+	console.log(a + b)
 	return a + b;
 
 }
 
-console.log(a + b)
-
 // soustraction
 
 function soustraction(a, b) {
+ 
+	console.log(a - b )
 	return a - b;
 }
-
-console.log(a - b )
 
 // miltiplication
 
 function multiplication(a, b) {
+	console.log(a * b);
 	return a * b;
 }
 
-console.log(a * b)
 
 // division
 
-function multiplication(a, b) {
- 	return a / b
- 
- if (b> 0) 
- 	console.log(a / b);
- else 
- 	console.log("Can't divide by 0")}
+function division(a, b) {
 
-console.log	(a / b);
+	console.log	(a / b);
+	return a / b;
+ 
+	if (b> 0) 
+ 	console.log(a / b);
+ 	else 
+ 	console.log("Can't divide by 0");
+}
