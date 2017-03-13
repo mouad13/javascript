5ebfb3ae1877724417e4a357ee18be4436e6943f
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-13 22:03:21 +0100

    calculatrice jquery

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // addition
 
 function addition(a, b) {
@@ -31,8 +30,39 @@ function division(a, b) {
 	console.log	(a / b);
 	return a / b;
  
-	if (b> 0) 
+	if (b > 0) {
  	console.log(a / b);
- 	else 
+ 	}
+
+ 	else {
  	console.log("Can't divide by 0");
+    }
+
 }
+
+var operation;
+var nombre1;
+var nombre2;
+
+
+$("#op").on("click", function(ev){
+     operation = this.value;
+
+})
+
+$("#egal").on("click", function(ev) {
+	operation = "resultat";
+	
+});
+
+$(".num").on("click", function(ev) {
+    nombre1 = this.value;
+    
+});
+
+$("button").on("click", function(ev) {
+	var valeur = $(this).val();
+	var valeur2 = $("#resultat").val();
+	var resultat = valeur + valeur2;
+	$("#resultat").val(resultat);
+});
